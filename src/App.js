9918d3dca011a9df19e0a208acdf32a12c7aa28e
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-17 21:20:05 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LoadCountries>
+        <Country></Country>
+      </LoadCountries> */}
+    </div>
+  );
+}
+
+export default App;
