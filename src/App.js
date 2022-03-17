639ca0ee97c5d5cf03fbb96a91ddb242ab18aee4
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-17 22:50:13 +0600

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import Countries from "./components/Country/Countries";
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries>
-        <Country></Country>
-      </LoadCountries>
+      <Countries>
+
+      </Countries>
     </div>
   );
 }
